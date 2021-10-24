5aed65276aa263d955fd97fe0ea7a590205691b1
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2021-10-24 15:38:51 -0300

    Aula02

---
+++ src/Aula02/Consumidores.java
@@ -0,0 +1,17 @@
+package Aula02;
+
+import java.util.function.Consumer;
+
+public class Consumidores {
+    public static void main(String[] args) {
+        //Method Reference
+
+//        - apenas
+//        - utilizar o parametro da forma que ele foi recebido;
+
+
+        Consumer<String> imprimirUmaFrase = System.out::println;
+        Consumer<String> imprimirUmaFrase2 = frase -> System.out.println(frase);
+        imprimirUmaFrase.accept("Hello World");
+    }
+}
